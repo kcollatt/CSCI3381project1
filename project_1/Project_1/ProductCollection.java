7da1f71578363eb7d136f52b4864c56845c966f5
kcollatt <kcollatt@cub.uca.edu>
2022-02-13 13:44:50 -0600

    just a save point

---
+++ project_1/Project_1/ProductCollection.java
@@ -0,0 +1,9 @@
+package Project_1;
+
+public class ProductCollection {
+
+	public ProductCollection() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- project_1/Project_1/Tester.java
+++ project_1/Project_1/Tester.java
@@ -1,6 +1,6 @@
 package Project_1;
 
-public class temp {
+public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
